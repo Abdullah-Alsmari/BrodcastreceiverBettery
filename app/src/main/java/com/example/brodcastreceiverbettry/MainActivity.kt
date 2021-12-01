aa515650dd9cd97de7704b988024bc2e5d4a1f7f
Abdulah-alsmari <Abdullah.Mohammed.ruh.k@tuwaiq.edu.sa>
2021-12-01 22:38:11 +0300

    add Charging and health battery

--- app/src/main/java/com/example/brodcastreceiverbettry/MainActivity.kt
+++ app/src/main/java/com/example/brodcastreceiverbettry/MainActivity.kt
@@ -6,22 +6,26 @@ import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.AppCompatTextView
+import android.os.BatteryManager
+
+import android.os.Build.VERSION_CODES
+
+import android.os.Build.VERSION
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
    lateinit var  myBatteryReciver: MyBatteryReceiver
-    lateinit var  MyReceiver2 : MyReceiver2
 
     @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textViewBL = findViewById<AppCompatTextView>(R.id.textViewBL)
-        val textViewBL2 = findViewById<AppCompatTextView>(R.id.healthBat)
-        myBatteryReciver = MyBatteryReceiver(textViewBL)
-        MyReceiver2 = MyReceiver2(textViewBL2)
-        registerReceiver(myBatteryReciver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
-        registerReceiver(myBatteryReciver, IntentFilter(Intent.ACTION_BATTERY_LOW))
-        registerReceiver(myBatteryReciver, IntentFilter(Intent.ACTION_BATTERY_OKAY))
+        val levelValue = findViewById<AppCompatTextView>(R.id.levelValue)
+        val isChargingValue = findViewById<AppCompatTextView>(R.id.isChargingValue)
+        val batteryHealthValue = findViewById<AppCompatTextView>(R.id.batteryHealthValue)
+        myBatteryReciver = MyBatteryReceiver(levelValue,isChargingValue,batteryHealthValue)
+        registerReceiver(myBatteryReciver,IntentFilter(Intent.ACTION_BATTERY_CHANGED))
 
 
     }
--- app/src/main/java/com/example/brodcastreceiverbettry/MyBatteryReceiver.kt
+++ app/src/main/java/com/example/brodcastreceiverbettry/MyBatteryReceiver.kt
@@ -6,18 +6,68 @@ import android.content.Intent
 import android.os.BatteryManager
 import android.telecom.TelecomManager
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatTextView
 
-class MyBatteryReceiver(val textView: AppCompatTextView) : BroadcastReceiver() {
+class MyBatteryReceiver(
+    val levelValue: AppCompatTextView,
+    val isChargingValue: TextView,
+    val batteryHealthValue: TextView
+) : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
-  val batteryLevel = intent?.getIntExtra("level",0)
-       textView.text = if (batteryLevel?:0>0)batteryLevel.toString()
-       else ""
 
+        val batteryLevel = intent?.getIntExtra("level", 0)
+        levelValue.text = if (batteryLevel ?: 0 > 0) batteryLevel.toString()
+        else ""
 
-
-
+        val status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1)
+        var BStatus = "No Data"
+        if (status == BatteryManager.BATTERY_STATUS_CHARGING) {
+            BStatus = "Charging"
         }
+        if (status == BatteryManager.BATTERY_STATUS_DISCHARGING) {
+            BStatus = "Discharging"
+        }
+        if (status == BatteryManager.BATTERY_STATUS_FULL) {
+            BStatus = "Full"
+        }
+        if (status == BatteryManager.BATTERY_STATUS_NOT_CHARGING) {
+            BStatus = "Not Charging"
+        }
+        if (status == BatteryManager.BATTERY_STATUS_UNKNOWN) {
+            BStatus = "Unknown"
+        }
+
+        isChargingValue.text = BStatus
+
+
+        val BHealth = intent.getIntExtra(BatteryManager.EXTRA_HEALTH, -1)
+        var BatteryHealth = "No Data"
+        if (BHealth == BatteryManager.BATTERY_HEALTH_COLD) {
+            BatteryHealth = "Cold"
+        }
+        if (BHealth == BatteryManager.BATTERY_HEALTH_DEAD) {
+            BatteryHealth = "Dead"
+        }
+        if (BHealth == BatteryManager.BATTERY_HEALTH_GOOD) {
+            BatteryHealth = "Good"
+        }
+        if (BHealth == BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE) {
+            BatteryHealth = "Over-Voltage"
+        }
+        if (BHealth == BatteryManager.BATTERY_HEALTH_OVERHEAT) {
+            BatteryHealth = "Overheat"
+        }
+        if (BHealth == BatteryManager.BATTERY_HEALTH_UNKNOWN) {
+            BatteryHealth = "Unknown"
+        }
+        if (BHealth == BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE) {
+            BatteryHealth = "Unspecified Failure"
+        }
+
+        batteryHealthValue.text = BatteryHealth
+
+
     }
+}
